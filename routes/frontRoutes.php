ab20a6dfbf6d2882f8503ff0c86bcf0f97b3edab
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-08-30 13:43:32 +0300

    frontRoute

---
+++ routes/frontRoutes.php
@@ -0,0 +1,8 @@
+<?php
+
+// example Controller names and functions
+// 'event' (key) is provided for the shortcode as attribute
+return [
+    'event' => 'EventController@index',
+    'gallery' => 'GalleryController@index',
+];
