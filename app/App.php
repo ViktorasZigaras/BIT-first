7dfd235b872217edb1e96d66b5a7b383f2daf488
ViktorasZigaras <44396727+ViktorasZigaras@users.noreply.github.com>
2020-09-02 13:15:06 +0300

    temp Routes

--- app/App.php
+++ app/App.php
@@ -2,6 +2,8 @@
 
 namespace BIT\app;
 
+use BIT\app\AdminRouter;
+
 // use Symfony\Component\HttpFoundation\Request;
 // require_once __DIR__.'/vendor/autoload.php';
 // use Symfony\Component\Finder\Finder;
@@ -19,7 +21,11 @@ class App {
 
     // construct priskiria savybes, kurie yra keliai
     public function __construct(){
-        $this->routeDir = plugin_dir_path(__FILE__).'routes/'; 
+        // temp Viktoro:
+        $generalDir = plugin_dir_path(__FILE__);
+        $this->routeDir = substr($generalDir, 0, strlen($generalDir) - 4,) . 'routes/'; 
+        // original Ievos:
+        // $this->routeDir = plugin_dir_path(__FILE__).'routes/';
         $this->viewDir = plugin_dir_path(__FILE__).'views/'; 
         $this->resourseDir = plugin_dir_path(__FILE__).'resources/'; 
         $this->apiUrl = plugin_dir_url(__FILE__).'api/'; // unused
@@ -29,7 +35,19 @@ class App {
     //sukuria naują objektą
     static public function start(){
         add_shortcode( 'front_shortcode', 'frontRoute' );
-        return new self;
+
+        // temp Viktoro:
+        // function load_admin_styles() {
+            // get exact css path
+            // wp_register_style( 'app.css', plugin_dir_path( __FILE__ ) . 'css/app.css');
+            // wp_enqueue_style( 'app.css');
+        // }
+
+        // temp Viktoro:
+        $app = new self;
+        new AdminRouter($app);
+
+        return $app;
     }
 
     // magic metodas, kuris leidžia prieiti prie privačios savybės
--- index.php
+++ index.php
@@ -9,35 +9,22 @@
 * Author URI: http://yourwebsiteurl.com/
 **/
 
-require_once __DIR__.'/vendor/autoload.php';
-// require_once __DIR__.'/app/App.php';
-
 use BIT\app\App;
 
-add_action('admin_menu', function() {
-    // add_menu_page('Title', 'Menu', 'manage_options', 'Path', 'render_event_function');
-
-    $app = App::start();
-    // 'admin' => 'AdminController@index'
-    // echo $app->routeDir;
-    // $routes = require $app->routeDir . 'adminRoute.php';
-    $routes = require 'routes/adminRoute.php';
-    foreach ($routes as $path => $route) {
-        list($controller, $function) = explode('@', $route, 2);
-        add_menu_page(ucfirst($path) . ' Title', ucfirst($path) . ' Menu', 'manage_options', $path, 'render' . $controller . '_' . $function);
-    }
-
-    // add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', string $icon_url = '', int $position = null );
-    // add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', int $position = null );
-
-    // add_submenu_page('bebras', 'Page title 2', 'Menu title 2', 'manage_options', 'bebras2', 'bebro_funkcija2');
-    // add_submenu_page('bebras', 'Edit', null, 'manage_options', 'bebras3', 'bebro_funkcija3');
-});
+// add_action('admin_menu', function() {
+//     $app = App::start();
+//     $routes = require $app->routeDir . 'adminRoute.php';
+//     // $routes = require '../routes/adminRoute.php';
+//     foreach ($routes as $path => $route) {
+//         list($controller, $function) = explode('@', $route, 2);
+//         add_menu_page(ucfirst($path) . ' Title', ucfirst($path) . ' Menu', 'manage_options', $path, 'render' . $controller . '_' . $function);
+//     }
+// });
 
 function renderAdminController_index() {
     echo 'Admin <br>';
     $app = App::start();
-    print_r($app);
+    // print_r($app);
     echo '<br> ' . $app->routeDir;
 }
 
@@ -48,3 +35,9 @@ function renderFrontController_index() {
 function renderApiController_index() {
     echo 'Api';
 }
+
+require_once __DIR__.'/vendor/autoload.php';
+
+// use BIT\app\App;
+
+App::start();
