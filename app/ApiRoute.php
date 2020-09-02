7bf10c03e1cfdc5f13230f6d0bb8546190fa87b2
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-09-02 15:53:21 +0300

    add namespace controller

--- app/ApiRoute.php
+++ app/ApiRoute.php
@@ -7,15 +7,16 @@ use BIT\app\App;
 class ApiRoute
 {
     function apiRoute() {
-        //$param - $_GET['route'] - /bebras/api/?route=labas
-        //$param = get_query_var( 'route', '' );
+        /**$get_param = $_GET['route'] - /bebras/api/?route=labas*/
+        //$param = get_query_var( 'route', '' );    // wp metodas
         $get_param = $request->query->get('route', '');
-        $app = App::start();        
+        $app = App::start();    //get App Singelton, kad nekurti naujo App objekto
         $routes = $app->routeDir.'apiRoutes.php';
         foreach ($routes as $key => $route) {
             if ($key == $get_param) {
                 list($controller, $method) = explode('@', $route);
-                return (new $controller)->$method();
+                $spacenameController = 'BIT\\controllers\\' . $controller;
+                return (new $spacenameController)->$method();
             }
         }
         
