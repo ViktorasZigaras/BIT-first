f960035de83f7030aab2ac7c76e8abfcee572099
ieva Skyriene <ieva.skyriene@gmail.com>
2020-09-03 12:58:50 +0300

    request

--- app/App.php
+++ app/App.php
@@ -1,34 +1,61 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
+require_once __DIR__.'/vendor/autoload.php';
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
-namespace BIT\app;
+// use Symfony\Component\Finder\Finder;
 
-class App
-{
+class App {
 
+    private $containerBuilder;
+    private $loader;
     private $routeDir;
     private $viewDir;
     private $resourseDir;
     private $apiUrl;
-    private $request;
 
-    // construct priskiria savybes, kurie yra keliai
-    public function __construct()
+    static private $obj;
+
+    public static function start()
     {
+        return self::$obj ?? self::$obj = new self;
+    }
+
+    //keiciam i private?
+    private function __construct(){
         $this->routeDir = PLUGIN_DIR_PATH . 'routes/';
         $this->viewDir = PLUGIN_DIR_PATH . 'views/';
         $this->resourseDir = PLUGIN_DIR_PATH . 'resources/';
         $this->apiUrl = PLUGIN_DIR_URL . 'api/'; // unused
+        $this->containerBuilder = new ContainerBuilder();
+        $this->loader = new PhpFileLoader($this->containerBuilder, new FileLocator(__DIR__));
+        $this->loader->load('services.php');
+        add_shortcode( 'front_shortcode', 'frontRoute' );
+        // $this->request = Request::createFromGlobals();
     }
 
+    //Julius duos konstantas - plugin_dir ir plugin_url
     //sukuria naują objektą
-    static public function start()
+    public function getService($service){
+        return  $this->containerBuilder->get($service);
+    }
+//paleisti
+// arba stat kaip singleton arba getApp() metoda, kad visi galetu gauti objekta. Arba is starto padaryti, kad jeigu sukurtas grazina, o jeigu ne, tai sukuria nauja. Starta i bendrini pavadinima.
+
+    public function run($controller, $method)
     {
-        add_action('admin_enqueue_scripts', function () {
-            wp_enqueue_style('app', PLUGIN_DIR_URL . 'public/style/app.css');
-            wp_enqueue_style('app');
-        });
-        add_shortcode('front_shortcode', 'frontRoute');
-        return new self;
+            // $this->controller = $this->routes->getController($route); //BebroController
+            // $this->method = $this->routes->getMethod($route); //bebras
+            $this->reflectionParams = (new \ReflectionMethod($this->controller, $this->method))->getParameters();
+ 
+            foreach ($this->reflectionParams as $val) {
+                if ($val->getType()) {
+                    $params[] = $this->getService($val->getType()->getName()); // kvieciu is konteinerio
+                }
+            } 
+            return (new $this->controller)->{$this->method}(...$params);
     }
 
     // magic metodas, kuris leidžia prieiti prie privačios savybės
