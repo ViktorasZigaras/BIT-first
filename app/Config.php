f54fedb3829197da8bd954fed63a85b069b7964e
PauliusLat <56875176+PauliusLat@users.noreply.github.com>
2020-09-07 14:21:14 +0300

    Config bugFix

--- app/Config.php
+++ app/Config.php
@@ -7,15 +7,15 @@ class Config{
 
         $types = require PLUGIN_DIR_PATH . 'configs/postTypeConfigs.php';
         if ($types) {
-            foreach ($types as $type1 => $args1) {
-                
-                add_action('init', function(){
-                    global $type1;
-                    global $args1;
-                    register_post_type($type1, $args1);
-                });   
-            }   
+            add_action('init', function() use($types){
+            foreach ($types as $type => $args) {
+                    register_post_type($type, $args);
+                }   
+            });   
         }
     }
+
+
+
 
 }
