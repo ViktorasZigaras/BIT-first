8ee52d362c5505401d62a64d70eb45315a4bb703
el Aiste <aiste.norei@gmail.com>
2020-09-07 13:45:51 +0300

    controller news

--- routes/adminRoute.php
+++ routes/adminRoute.php
@@ -2,4 +2,9 @@
 
 return [
     'admin' => 'AdminController@index',
+    'news' => 'NewsController@index',
+    'news' => 'NewsController@create',
+    'news' => 'NewsController@update',
+    'news' => 'NewsController@delete',
+    'philosophy' => 'PhilosophyController@index'
 ];
