3fa6222ffad592676248485aa40f23921ffd7223
ieva Skyriene <ieva.skyriene@gmail.com>
2020-09-10 13:17:38 +0300

    service

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -3,6 +3,7 @@
 namespace BIT\controllers;
 
 use BIT\app\App;
+use BIT\app\View;
 
 use Symfony\Component\HttpFoundation\Request;
 use BIT\models\NewsPost;
@@ -13,19 +14,19 @@ class NewsController {
     {   
 
         $news = NewsPost::all();
-        return view('news.index');
+        View::adminRender('news.index', ['url' => PLUGIN_DIR_URL, 'news']);
         
     }
 
     public function create()
     {
-        return view('news.create');
+        return view('news.create'); 
     }
-
-
-    public function store(Request $request)
+    
+    
+    public function store(Request $request, NewsPost $newsPost)
     {   
-        
+        _dc($newsPost);
         $new_news = new NewsPost();
         $new_news->news_content = $request->content;
         // $new_news->= $request->menu_surname;
@@ -58,6 +59,8 @@ class NewsController {
     public function update(Request $request, NewsPost $newsPost)
     {   
         $newsPost->news_content = $request->content;
+
+
         // // $menu->surname = $request->surname;
         // if ($request->hasFile('photo')) {
         //     $image = $request->file('photo');
